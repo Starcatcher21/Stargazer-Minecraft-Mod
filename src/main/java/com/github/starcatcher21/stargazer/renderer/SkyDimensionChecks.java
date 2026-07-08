5def518bd2729a3ea2e942a2670d894d3a1a4e5a
wall5009 <rolandjlindenfelser@gmail.com>
2026-07-08 10:38:07 -0700

    new skybox (yay)

---
+++ src/main/java/com/github/starcatcher21/stargazer/renderer/SkyDimensionChecks.java
@@ -0,0 +1,22 @@
+package com.github.starcatcher21.stargazer.renderer;
+
+import com.github.starcatcher21.stargazer.CustomWorlds;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.dimension.DimensionType;
+
+import java.util.Optional;
+
+public final class SkyDimensionChecks {
+    private SkyDimensionChecks() {
+    }
+
+    public static boolean isStargazerSkyDimension(Level level) {
+        Optional<ResourceKey<DimensionType>> dimensionType = level.dimensionTypeRegistration().unwrapKey();
+
+        return dimensionType.isPresent()
+                && (dimensionType.get().equals(CustomWorlds.COSMIC_TYPE)
+                || dimensionType.get().equals(CustomWorlds.RED_ORB_TYPE)
+                || dimensionType.get().equals(CustomWorlds.WANDER_TYPE));
+    }
+}
