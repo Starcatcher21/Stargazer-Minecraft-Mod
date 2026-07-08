487deafce101bad6cfb631b37c3f5545ff067ad3
wilwe <wilwe036@gmail.com>
2026-07-08 21:20:50 +0200

    removed starquads why i need more quads

--- src/main/java/com/github/starcatcher21/stargazer/renderer/StarQuads.java
+++
@@ -1,8 +0,0 @@
-package com.github.starcatcher21.stargazer.renderer;
-
-import com.mojang.blaze3d.vertex.BufferBuilder;
-import net.minecraft.world.phys.Vec3;
-import org.joml.Matrix4fc;
-
-public class StarQuads {
-}
